51b5ed738e6fbf6a46ca2bd9207ed434add0b10f
romirahmann <romirahman03romi@gmail.com>
2026-01-28 10:03:26 +0700

    DB_CONFIG

--- src/lib/db.config.js
+++ src/lib/db.config.js
@@ -3,8 +3,8 @@ import knex from "knex";
 const db = knex({
   client: "mysql2",
   connection: {
-    host: process.env.DB_HOST,
-    port: Number(process.env.DB_PORT),
+    host: process.env.DB_HOST || "localhost",
+    port: Number(process.env.DB_PORT || 3306),
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
